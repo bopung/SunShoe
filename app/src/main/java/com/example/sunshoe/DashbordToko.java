d00a58ab7605d7320eec6831147bc0f834a34bbd
Jurgen <loajurgen11@gmail.com>
2022-11-22 00:59:10 +0800

    baru layout awal

---
+++ app/src/main/java/com/example/sunshoe/DashbordToko.java
@@ -0,0 +1,14 @@
+package com.example.sunshoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashbordToko extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashbord_toko);
+    }
+}
