ba6ae7db969cf95244d40b1cc5e5674ea3f695de
bopung <stevanuspungky@gmail.com>
2022-11-24 19:41:26 +0700

    login jalan ya

--- app/src/main/java/com/example/sunshoe/Register.java
+++ app/src/main/java/com/example/sunshoe/Register.java
@@ -1,14 +1,180 @@
 package com.example.sunshoe;
 
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+
+    private EditText name, pass;
+    private String action_flag="add";
+    private String refreshFlag="0";
+    private static final String TAG="RegistActivity";
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+//        button untuk save data ke server
+        Button buttonRegist = findViewById(R.id.buttonRegist);
+        buttonRegist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveDataVolley();
+            }
+        });
+
+
+        initUI();
+        Intent intent = getIntent();
+
+        TextView textLogin = findViewById(R.id.textLogin);
+        textLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void initUI() {
+        pDialog = new ProgressDialog(Register.this);
+
+        name   = (EditText) findViewById(R.id.name);
+        pass  = (EditText) findViewById(R.id.pass);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.buttonRegist) {
+            saveDataVolley();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void finish() {
+        System.gc();
+        Intent data = new Intent();
+        data.putExtra("refreshflag", refreshFlag);
+        setResult(RESULT_OK, data);
+        super.finish();
+    }
+
+    //    metode untuk save ke server menggunakan volley
+    private void saveDataVolley(){
+        refreshFlag="1";
+        final String Username = name.getText().toString();
+        final String Password = pass.getText().toString();
+
+        String url = AppConfig.REGIST_URL;
+        pDialog.setMessage("Menyimpan Data...");
+        showDialog();
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        hideDialog();
+                        Log.d("Register", "response :" + response);
+                        // Toast.makeText(getBaseContext(),"response: "+response, Toast.LENGTH_SHORT).show();
+                        processResponse("Save Data",response);
+                        finish();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        hideDialog();
+                        error.printStackTrace();
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<>();
+                // the POST parameters:
+
+                params.put("username",Username);
+                params.put("password",Password);
+                if (action_flag.equals("add")){
+                    params.put("id","0");
+                }else{
+
+                }
+                return params;
+            }
+        };
+        Volley.newRequestQueue(this).add(postRequest);
+
+    }
+
+    private void processResponse(String paction, String response){
+
+        try {
+            JSONObject jsonObj = new JSONObject(response);
+            String errormsg = jsonObj.getString("errormsg");
+            Toast.makeText(getBaseContext(),paction+" "+errormsg,Toast.LENGTH_SHORT).show();
+
+        } catch (JSONException e) {
+            Log.d("Register", "Server Unreachable !");
+        }
+
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
