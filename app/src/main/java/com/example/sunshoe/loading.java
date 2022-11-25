2df90f52d3232192417f2dc2b51bd3b7bb166b4d
vincentjbudiman <vinbud777@gmail.com>
2022-11-25 13:17:35 +0700

    loading nih boss

--- app/src/main/java/com/example/sunshoe/loading.java
+++ app/src/main/java/com/example/sunshoe/loading.java
@@ -2,7 +2,11 @@ package com.example.sunshoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class loading extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class loading extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        time();
+    }
+
+    private void time() {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(loading.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
     }
 }
